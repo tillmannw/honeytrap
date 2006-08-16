9c5f7fbf2888f801413046fe30d057412124a837
till <till@carnivore.it>
2006-08-16 10:31:35 +0000

    tools added

---
+++ tools/base64decode.c
@@ -0,0 +1,145 @@
+/* base64decode.c
+ * Copyright (C) 2006 Tillmann Werner <tillmann.werner@gmx.de>
+ *
+ * This file is free software; as a special exception the author gives
+ * unlimited permission to copy and/or distribute it, with or without
+ * modifications, as long as this notice is preserved.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * This file is part of the honeytrap tools collection.
+ * 
+ * base64decode maps a file into memory, scans it for characteristic
+ * strings and tries to base64-decode parts of it.
+ *
+ * The decoded message is printed to stdout.
+ * 
+ * Use gcc and compile with   -O -fforce-mem -frerun-loop-opti
+ * to improve performace.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <strings.h>
+
+struct dec {
+	char *str;
+	int len;
+};
+
+struct dec decode(const char* code, int len) {
+	u_char ch, inbuf[3], outbuf[4];
+	int charctr, bufctr, ign, eot, i;
+	struct dec ret;
+
+	eot	= 0;
+	ign	= 0;
+	bufctr	= 0;
+	ret.len	= 0;
+
+	ret.str  = (u_char*) malloc(len*3/4+1);
+	bzero(ret.str, len*3/4+1);
+
+	for (i=0; i<len; i++) {
+		ch = code[i];
+
+		if isupper(ch) ch -= 'A';
+		else if islower(ch) ch = ch - 'a' + 26;
+		else if isdigit(ch) ch = ch - '0' + 52;
+		else if (ch == '+') ch = 62;
+		else if (ch == '=') eot = 1;
+		else if (ch == '/') ch = 63;
+		else ign = 1;
+
+		if (!ign) {
+			if (eot) {
+				if (bufctr == 0) return;
+				charctr = ((bufctr == 1) || (bufctr == 2)) ? 1 : 2;
+				bufctr = 3;
+			} else charctr = 3;
+
+			inbuf[bufctr++] = ch;
+
+			if (bufctr == 4) {
+				bufctr = 0;
+
+				ret.str[ret.len++] =  (inbuf[0] << 2) | ((inbuf[1] & 0x30) >> 4);
+				if (charctr > 0) ret.str[ret.len++] =  ((inbuf[1] & 0x0F) << 4) | ((inbuf[2] & 0x3C) >> 2);
+				if (charctr > 1) ret.str[ret.len++] =  ((inbuf[2] & 0x03) << 6) | (inbuf[3] & 0x3F);
+			}
+			if (eot) return(ret);
+		}
+	}
+	return(ret);
+}
+
+int main(int argc, char *argv[]) {
+	int fd, bytes_read, i;
+	struct stat filestat;
+	u_char *content, *code, v, in[4], out[3];
+	struct dec decoded;
+
+	if (argc < 2) {
+		fprintf(stderr, "Errror - No filename given.\n");
+		exit(1);
+	}
+
+	/* open file */
+	if ((fd = open(argv[1], O_RDONLY)) == -1) {
+		fprintf(stderr, "Error - Unable to open file: %s.\n", strerror(errno));
+		exit(1);
+	}
+
+	/* get file size */
+	if (fstat(fd, &filestat) != 0) {
+		fprintf(stderr, "Error - Unable to get file size: %s.\n", strerror(errno));
+		exit(1);
+	}
+	if (filestat.st_size < 1) {
+		fprintf(stdout, "File is empty.\n");
+		exit(0);
+	}
+
+	/* map file content into memory */
+	if ((content = (u_char *) malloc(filestat.st_size)) == NULL) {
+		fprintf(stderr, "Error - Unable to allocate memory: %s.\n", strerror(errno));
+		exit(1);
+	}
+	bzero(content, filestat.st_size);
+	bytes_read = 0;
+	if ((bytes_read = read(fd, content+bytes_read, filestat.st_size)) < filestat.st_size) {
+		if (bytes_read  == -1) {
+			fprintf(stderr, "Error - Unable to map file into memory: %s.\n", strerror(errno));
+			exit(1);
+		}
+		if (bytes_read == 0) {
+			fprintf(stderr, "Error - EOF reached too early.\n");
+			exit(1);
+		}
+	}
+
+	/* look for 'Negotiate ' in content, base64 code starts after it */
+	/* checks for other characteristic strings can be done here as well */
+	if ((code = strstr(content, "Negotiate "))) code += 10;
+	else code = content;
+
+	/* decode base64 code */
+	decoded = decode((char *)code, bytes_read);
+
+	/* print it to stdout */
+	for (i=0; i<decoded.len; fprintf(stdout, "%c", decoded.str[i++]));
+
+	free(decoded.str);
+	free(content);
+	close(fd);
+}
---
+++ tools/edist.c
@@ -0,0 +1,127 @@
+/* edist.c
+ * Copyright (C) 2006 Tillmann Werner <tillmann.werner@gmx.de>
+ *
+ * This file is free software; as a special exception the author gives
+ * unlimited permission to copy and/or distribute it, with or without
+ * modifications, as long as this notice is preserved.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * This file is part of the honeytrap tools collection.
+ * 
+ * edist calculates the edit distance (aka levenshtein distance) for two
+ * arbitrary (text or binary) files and tells the percentage of similarity
+ * and the absolute value. The algorithm variant used here uses linear space.
+ *
+ * The edit distance can be used as a metric for binary file comparison.
+ * Further information can be found on
+ * <http://en.wikipedia.org/wiki/Levenshtein_distance>.
+ * 
+ * Use gcc and compile with   -O -fforce-mem -frerun-loop-opti
+ * to improve performace.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define COST_INS	1
+#define COST_DEL	1
+#define COST_REP	1
+#define min(a, b) ((a) < (b) ? a : b)
+#define max(a, b) ((a) > (b) ? a : b)
+
+
+struct bstr {
+	u_char *str;
+	u_int32_t len;
+};
+
+
+/* load data into memory */
+struct bstr file2string(const char *filename) {
+	int bytes_read, fd;
+	u_char buffer[BUFSIZ];
+	struct bstr bstring;
+
+	bstring.len	= 0;
+	bstring.str	= NULL;
+	
+	if ((fd = open(filename, 0)) == -1) {
+		fprintf(stderr, "Unable to open %s: %s.\n", filename, strerror(errno));
+		exit(1);
+	}
+	while ((bytes_read = read(fd, buffer, BUFSIZ)) > 0) {
+		if ((bstring.str = (void *) realloc(bstring.str, bstring.len + bytes_read)) == NULL) {
+			fprintf(stderr, "Unable to allocate memory: %s.\n", strerror(errno));
+			exit(1);
+		}
+		memcpy(bstring.str + bstring.len, buffer, bytes_read);
+		bstring.len += bytes_read;
+	}
+	if (bytes_read < 0) {
+		fprintf(stderr, "Unable to read from %s: %s.\n", filename, strerror(errno));
+		exit(1);
+	}
+	close(fd);
+	return(bstring);
+}
+
+
+/* calculate and return edit distance */
+u_int32_t edit_dist(struct bstr str1, struct bstr str2) {
+	register u_int32_t i, j;
+	u_int32_t *p, *q, *r;
+
+	if ((p = (uint *) calloc(str1.len, sizeof(uint))) == NULL) {
+		fprintf(stderr, "Unable to allocate memory: %s.\n", strerror(errno));
+		exit(1);
+	}
+	if ((q = (uint *) calloc(str2.len, sizeof(uint))) == NULL) {
+		fprintf(stderr, "Unable to allocate memory: %s.\n", strerror(errno));
+		exit(1);
+	}
+	p[0] = 0;
+
+	for(j=1; j<str2.len; ++j) p[j] = p[j-1] + COST_INS;
+
+	for(i=1; i<str1.len; ++i) {
+		q[0] = p[0] + COST_DEL;
+		for(j=1; j<str2.len; ++j) 
+			q[j] = min(min(p[j]+COST_DEL, q[j-1]+COST_INS),
+				p[j-1]+(str1.str[i-1] == str2.str[j-1] ? 0 : COST_REP));
+
+		r = p;
+		p = q;
+		q = r;
+	}
+	return(p[str2.len-1]);
+}
+
+
+/* calculate similarity of binary files (in percent) via edit distance */
+int main(int argc, char *argv[]) {
+	struct bstr bstr1, bstr2;
+	u_int32_t dist;
+	float eq;
+
+	if (argc < 3) printf("usage: %s file1 file2\n", argv[0]);
+	else {
+		bstr1	= file2string(argv[1]);
+		bstr2	= file2string(argv[2]);
+
+		dist	= edit_dist(bstr1, bstr2);
+		eq	= dist*100;
+		eq	= 100-eq/max(bstr1.len, bstr2.len);
+
+		printf("Similarity: %.2f%% (edit distance: %u).\n", eq, dist);
+	}
+	return(0);
+}
